29b2c2ed4b52e392c1fcbdec2bb715e7941e74b3
JulioNunesDev <julionunes999@gmail.com>
2022-04-27 20:33:18 -0300

    feat: bugs

--- src/shared/services/ApiCategory.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-
-const ApiKey = import.meta.env.VITE_SOME_KEY
-
-const ApiCategory = axios.create({
-    baseURL: `https://newsapi.org/v2/top-headlines?country=br&category=technology&apiKey=${ApiKey}`
-}) 
